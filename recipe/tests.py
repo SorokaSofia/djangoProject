474c428036d8d589cd0c9a313a92ae6769f7b74e
SorokaSofia <s.soroka31@knu.ua>
2024-05-31 18:37:39 +0300

    pytest

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,26 +1,61 @@
 from django.test import TestCase
-from .models import Category, Recipe
+from django.urls import reverse
+from .models import Recipe, Category
 
-class CategoryModelTest(TestCase):
 
-    def setUp(self):
-        self.category = Category.objects.create(name="Desserts")
-
-    def test_category_creation(self):
-        self.assertIsInstance(self.category, Category)
-        self.assertEqual(self.category.__str__(), self.category.name)
-
-class RecipeModelTest(TestCase):
+class MainViewTests(TestCase):
 
     def setUp(self):
-        self.category = Category.objects.create(name="Desserts")
-        self.recipe = Recipe.objects.create(
-            name="Chocolate Cake",
-            description="Delicious chocolate cake recipe",
-            category=self.category
-        )
+        # Создаем категорию для рецептов
+        self.category = Category.objects.create(name='Desserts')
 
-    def test_recipe_creation(self):
-        self.assertIsInstance(self.recipe, Recipe)
-        self.assertEqual(self.recipe.__str__(), self.recipe.name)
-        self.assertEqual(self.recipe.category.name, "Desserts")
+        # Создаем несколько объектов Recipe для тестирования
+        for i in range(15):
+            Recipe.objects.create(
+                title=f'Recipe {i}',
+                description='Sample description',
+                instructions='Sample instructions',
+                ingredients='Sample ingredients',
+                category=self.category
+            )
+
+    def test_main_view_status_code(self):
+        response = self.client.get(reverse('recipe:main'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_view_template_used(self):
+        response = self.client.get(reverse('recipe:main'))
+        self.assertTemplateUsed(response, 'recipe/main.html')
+
+    def test_main_view_context(self):
+        response = self.client.get(reverse('recipe:main'))
+        self.assertTrue('recipes' in response.context)
+        self.assertEqual(len(response.context['recipes']), 10)
+
+
+class CategoryDetailViewTests(TestCase):
+
+    def setUp(self):
+        # Создаем категорию и несколько объектов Recipe для тестирования
+        self.category = Category.objects.create(name='Desserts')
+        for i in range(5):
+            Recipe.objects.create(
+                title=f'Recipe {i}',
+                description='Sample description',
+                instructions='Sample instructions',
+                ingredients='Sample ingredients',
+                category=self.category
+            )
+
+    def test_category_detail_view_status_code(self):
+        response = self.client.get(reverse('recipe:category_detail', args=[self.category.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_view_template_used(self):
+        response = self.client.get(reverse('recipe:category_detail', args=[self.category.id]))
+        self.assertTemplateUsed(response, 'recipe/category_detail.html')
+
+    def test_category_detail_view_context(self):
+        response = self.client.get(reverse('recipe:category_detail', args=[self.category.id]))
+        self.assertTrue('category' in response.context)
+        self.assertEqual(len(response.context['category']), 5)
