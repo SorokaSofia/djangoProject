6f4d2d6be1f663277642444e6231b2080d66e329
SorokaSofia <s.soroka31@knu.ua>
2024-05-31 17:38:09 +0300

    changes

--- recipe/views.py
+++ recipe/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, redirect, get_object_or_404
 def main(request):
     return render(request, 'recipe/main.html')
 
-def category_detail(request, category_slug):
+def category_detail(request):
     return render(request, 'recipe/category_detail.html')
